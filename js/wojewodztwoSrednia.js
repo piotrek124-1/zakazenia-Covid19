00f0ec9ce01e5b072a9e3623caa6b5aa88ec4d40
Piotr Mikuła <piotrek124@gmail.com>
2021-05-05 16:50:55 +0200

    added plots

---
+++ js/wojewodztwoSrednia.js
@@ -0,0 +1,86 @@
+var data = ["Cały kraj","dolnośląskie","kujawsko-pomorskie","lubelskie","lubuskie","łódzkie","małopolskie","mazowieckie","opolskie","podkarpackie","podlaskie","pomorskie","śląskie","świętokrzyskie","warmińsko-mazurskie","wielkopolskie","zachodniopomorskie"]
+var newPowiat = document.getElementById("wojewodztwoList")
+for (i in data) {
+    newPowiat = document.getElementById("wojewodztwoList").appendChild(document.createElement("option"))
+    newPowiat.value = data[i]
+}
+newPowiat = document.getElementById("wojewodztwoSelect")
+newPowiat.addEventListener("input", plot)
+document.getElementById("wojewodztwoSelect2").addEventListener("input", plot)
+document.getElementById("wojewodztwoSelect3").addEventListener("input", plot)
+var csv = false
+var plotCsvData = []
+if (csv === false) {
+    csvData = Plotly.d3.csv("https://raw.githubusercontent.com/piotrek124-1/zakazenia-Covid19/main/plotData/wojewodztwa_zakazenia.csv", function (error, data) {
+        plotCsvData = data
+        csv = true
+    })
+}
+var liczbaPrzypadkow = [], stan_rekordu_na = [], wojewodztwoName
+
+function plot() {
+    function activeWojewodztwo(wojewodztwo) {
+        liczbaPrzypadkow = []
+        stan_rekordu_na = []
+        var sum = 0
+        var j = 0
+        var index = []
+        for (i in plotCsvData) {
+            if (plotCsvData[i]["wojewodztwo"] === wojewodztwo) {
+                index.push(i)
+                sum += Number(plotCsvData[i]["liczba_przypadkow"])
+                if (j > 5) {
+                    liczbaPrzypadkow.push(sum / 7)
+                    stan_rekordu_na.push(plotCsvData[i]["stan_rekordu_na"])
+                    sum -= Number(plotCsvData[index[0]]["liczba_przypadkow"])
+                    index.shift()
+                    console.log(index)
+                }
+                j++
+            }
+        }
+        wojewodztwoName = wojewodztwo
+    }
+
+    activeWojewodztwo(document.getElementById("wojewodztwoSelect").value)
+    var trace = {
+        x: stan_rekordu_na,
+        y: liczbaPrzypadkow,
+        mode: "lines",
+        name: wojewodztwoName
+    }
+    activeWojewodztwo(document.getElementById("wojewodztwoSelect2").value)
+    var trace2 = {
+        x: stan_rekordu_na,
+        y: liczbaPrzypadkow,
+        mode: "lines",
+        name: wojewodztwoName
+    }
+    activeWojewodztwo(document.getElementById("wojewodztwoSelect3").value)
+    var trace3 = {
+        x: stan_rekordu_na,
+        y: liczbaPrzypadkow,
+        mode: "lines",
+        name: wojewodztwoName
+    }
+    var tick = trace.x
+    if (trace.x.length === 0) {
+        if (trace2.x.length === 0) {
+            tick = trace3.x
+        } else {
+            tick = trace2.x
+        }
+    }
+    var layout = {
+        title: "Wykres zakażeń Covid-19",
+        xaxis: {
+            title: "data",
+            tickvals: tick,
+            tickformat: "%d/%m"
+        }, yaxis: {
+            title: "Liczba przypadków (średnia 7 dniowa)"
+        }
+    }
+    var plotData = [trace, trace2, trace3]
+    Plotly.newPlot("plot", plotData, layout)
+}
